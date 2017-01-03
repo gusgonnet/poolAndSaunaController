0f396b09429644745fc2420b0e7f736f815f9dc0
ceajog <gustavo.gonnet@ericsson.com>
2017-01-03 00:36:21 -0500

    * changes in version 0.05:        * duplicating FSM and others to control pool and sauna

--- poolAndSaunaController.ino
+++ poolAndSaunaController.ino
@@ -35,7 +35,7 @@
 #include "FiniteStateMachine.h"
 
 #define APP_NAME "poolAndSaunaController"
-String VERSION = "Version 0.04";
+String VERSION = "Version 0.05";
 
 SYSTEM_MODE(AUTOMATIC);
 
@@ -51,6 +51,8 @@ SYSTEM_MODE(AUTOMATIC);
        * added turnOffAllRelays at boot time
  * changes in version 0.04:
        * Storing settings in EEPROM
+ * changes in version 0.05:
+       * duplicating FSM and others to control pool and sauna
 *******************************************************************************/
 
 // Argentina time zone GMT-3
@@ -75,8 +77,8 @@ State initState = State(initEnterFunction, initUpdateFunction, initExitFunction)
 State offState = State(offEnterFunction, offUpdateFunction, offExitFunction);
 State onState = State(onEnterFunction, onUpdateFunction, onExitFunction);
 
-//initialize state machine, start in state: init
-FSM pumpStateMachine = FSM(initState);
+//initialize state machine for the pool, start in state: init
+FSM stateMachine1 = FSM(initState);
 
 // Sample FSM every now and then - 100 milliseconds means 10 times per second
 #define QUICK_LOOP_INTERVAL 100
@@ -115,18 +117,33 @@ elapsedMillis temperatureSampleInterval;
 double temperatureCurrent = INVALID;
 double temperatureTarget = 30.0;
 
+//sensor difference with real temperature (if none set to zero)
+//use this variable to align measurements with your existing displays
+double temperatureCalibration = 0;
+
 //you can change this to your liking
 // a smaller value will make your temperature more constant at the price of
 //  starting the pump more often
 // a larger value will reduce the number of times the pump turns on but will leave it on a longer time
 double temperatureMargin = 0.25;
 
-//sensor difference with real temperature (if none set to zero)
-//use this variable to align measurements with your existing displays
-double temperatureCalibration = 0;
-
 // Celsius is the default unit, set this boolean to true if you want to use Fahrenheit
 const bool useFahrenheit = false;
+
+/*******************************************************************************
+ declare second FSM, with second sensor for the sauna
+*******************************************************************************/
+State initState2 = State(initEnterFunction2, initUpdateFunction2, initExitFunction2);
+State offState2 = State(offEnterFunction2, offUpdateFunction2, offExitFunction2);
+State onState2 = State(onEnterFunction2, onUpdateFunction2, onExitFunction2);
+//initialize state machine for the sauna, start in state: init
+FSM stateMachine2 = FSM(initState2);
+//Sets Pin D3 for Temp Sensor2
+DS18B20 ds18b20_2 = DS18B20(D3);
+double temperatureCurrent2 = INVALID;
+double temperatureTarget2 = 37.0;
+double temperatureCalibration2 = 0;
+String state2 = STATE_INIT;
 
 /*******************************************************************************
  relay variables
@@ -140,7 +157,8 @@ NCD4Relay relayController;
 //randomly chosen value here. The only thing that matters is that it's not 255
 // since 255 is the default value for uninitialized eeprom
 // value 137 will be used in version 0.4
-#define EEPROM_VERSION 137
+// value 138 will be used in version 0.5
+#define EEPROM_VERSION 138
 #define EEPROM_ADDRESS 0
 
 struct EepromMemoryStructure
@@ -148,6 +166,8 @@ struct EepromMemoryStructure
   uint8_t version = EEPROM_VERSION;
   double temperatureTarget;
   double temperatureCalibration;
+  double temperatureTarget2;
+  double temperatureCalibration2;
 };
 EepromMemoryStructure eepromMemory;
 
@@ -167,7 +187,6 @@ void setup()
   Particle.variable("temperature", temperatureCurrent);
   Particle.variable("target", temperatureTarget);
   Particle.variable("calibration", temperatureCalibration);
-
   // This variable informs the state of the system
   Particle.variable("state", state);
 
@@ -176,6 +195,16 @@ void setup()
   // Up to 15 cloud functions may be registered and each function name is limited to a maximum of 12 characters.
   Particle.function("setTarget", setTarget);
   Particle.function("setCalbrtion", setCalibration);
+
+  /*******************************************************************************
+   cloud variables and functions for the second FSM, with second sensor
+  *******************************************************************************/
+  Particle.variable("temperature2", temperatureCurrent2);
+  Particle.variable("target2", temperatureTarget2);
+  Particle.variable("calibration2", temperatureCalibration2);
+  Particle.variable("state2", state2);
+  Particle.function("setTarget2", setTarget2);
+  Particle.function("setCalbrtn2", setCalibration2);
 
   Time.zone(TIME_ZONE);
 
@@ -215,12 +244,13 @@ void quickLoop()
   // time is up, reset timer
   quickLoopTimer = 0;
 
-  // read the temperature sensor
+  // read the temperature sensors
   readTemperature();
 
-  // update the FSM
+  // update the FSMs
   // the FSM is the heart of the program - all actions are defined by its states
-  pumpStateMachine.update();
+  stateMachine1.update();
+  stateMachine2.update();
 }
 
 /*******************************************************************************
@@ -251,7 +281,7 @@ int setTarget(String parameter)
     return 0;
   }
 
-  Particle.publish(APP_NAME, "Failed to set termperature target to " + parameter + " (0<=value<=125)", PRIVATE);
+  Particle.publish(APP_NAME, "Failed to set temperature target to " + parameter + " (0<=value<=125)", PRIVATE);
   return -1;
 }
 
@@ -289,6 +319,63 @@ int setCalibration(String parameter)
 }
 
 /*******************************************************************************
+ * Function Name  : setTarget2
+ * Description    : this function sets the target temperature for the sauna
+ * Parameters     : String parameter: the target temperature
+                     Value has to be between 0 and 125. Anything else is ignored
+ * Return         : 0 if success, -1 if fails
+ *******************************************************************************/
+int setTarget2(String parameter)
+{
+
+  double localValue = atoi(parameter);
+
+  if ((localValue >= 0) && (localValue <= 125))
+  {
+    temperatureTarget2 = localValue;
+    Particle.publish(APP_NAME, "Setting temperature2 target to " + double2string(temperatureTarget2), PRIVATE);
+    saveSettingsInEeprom();
+    return 0;
+  }
+
+  Particle.publish(APP_NAME, "Failed to set temperature2 target to " + parameter + " (0<=value<=125)", PRIVATE);
+  return -1;
+}
+
+/*******************************************************************************
+ * Function Name  : setCalibration2
+ * Description    : this function sets the calibration of the temperature2
+ * Parameters     : String parameter: the calibration value
+                     Value has to be between -50 and 50. Anything else is ignored
+ * Return         : 0 if success, -1 if fails
+ *******************************************************************************/
+int setCalibration2(String parameter)
+{
+
+  double localValue = atoi(parameter);
+
+  if ((localValue >= -50) && (localValue <= 50))
+  {
+
+    // rollback previous calibration adjustment
+    temperatureCurrent2 = temperatureCurrent2 - temperatureCalibration2;
+
+    // store new calibration value
+    temperatureCalibration2 = localValue;
+
+    // apply new calibration adjustment
+    temperatureCurrent2 = temperatureCurrent2 + temperatureCalibration2;
+
+    Particle.publish(APP_NAME, "Setting temperature2 calibration to " + double2string(temperatureCalibration2), PRIVATE);
+    saveSettingsInEeprom();
+    return 0;
+  }
+
+  Particle.publish(APP_NAME, "Failed to set temperature2 calibration to " + parameter + " (-50<=value<=50)", PRIVATE);
+  return -1;
+}
+
+/*******************************************************************************
 ********************************************************************************
 ********************************************************************************
  SENSOR FUNCTIONS
@@ -298,7 +385,7 @@ int setCalibration(String parameter)
 
 /*******************************************************************************
  * Function Name  : readTemperature
- * Description    : reads the temperature sensor at every TEMPERATURE_SAMPLE_INTERVAL
+ * Description    : reads the temperature sensor in D2 and D3 at every TEMPERATURE_SAMPLE_INTERVAL
  * Return         : none
  *******************************************************************************/
 void readTemperature()
@@ -314,27 +401,17 @@ void readTemperature()
   temperatureSampleInterval = 0;
 
   getTemp();
+  getTemp2();
 
   Particle.publish(APP_NAME, "Temperature: " + double2string(temperatureCurrent), PRIVATE);
+  Particle.publish(APP_NAME, "Temperature2: " + double2string(temperatureCurrent2), PRIVATE);
 }
 
 /*******************************************************************************
- * Function Name  : double2string
- * Description    : return the string representation of the double number
-                     passed as parameter with 2 decimals
- * Return         : the string
+ * Function Name  : getTemp
+ * Description    : reads the DS18B20 sensor
+ * Return         : nothing
  *******************************************************************************/
-String double2string(double doubleNumber)
-{
-  String stringNumber = String(doubleNumber);
-
-  //return only 2 decimals
-  // Example: show 19.00 instead of 19.000000
-  stringNumber = stringNumber.substring(0, stringNumber.length() - 4);
-
-  return stringNumber;
-}
-
 void getTemp()
 {
 
@@ -375,6 +452,50 @@ void getTemp()
 }
 
 /*******************************************************************************
+ * Function Name  : getTemp2
+ * Description    : reads the second DS18B20 sensor
+ * Return         : nothing
+ *******************************************************************************/
+void getTemp2()
+{
+
+  int dsAttempts = 0;
+  double temperatureLocal = INVALID;
+
+  if (!ds18b20_2.search())
+  {
+    ds18b20_2.resetsearch();
+    temperatureLocal = ds18b20_2.getTemperature();
+    while (!ds18b20_2.crcCheck() && dsAttempts < 4)
+    {
+      dsAttempts++;
+      if (dsAttempts == 3)
+      {
+        delay(1000);
+      }
+      ds18b20_2.resetsearch();
+      temperatureLocal = ds18b20_2.getTemperature();
+      continue;
+    }
+    dsAttempts = 0;
+
+    if (useFahrenheit)
+    {
+      temperatureLocal = ds18b20_2.convertToFahrenheit(temperatureLocal);
+    }
+
+    // calibrate values
+    temperatureLocal = temperatureLocal + temperatureCalibration2;
+
+    // if reading is valid, take it
+    if ((temperatureLocal != INVALID) && (ds18b20_2.crcCheck()))
+    {
+      temperatureCurrent2 = temperatureLocal;
+    }
+  }
+}
+
+/*******************************************************************************
 ********************************************************************************
 ********************************************************************************
  FINITE STATE MACHINE FUNCTIONS
@@ -396,12 +517,12 @@ void initEnterFunction()
 void initUpdateFunction()
 {
   // is time up? no, then come back later
-  if (pumpStateMachine.timeInCurrentState() < (initTimeout * MILLISECONDS_TO_SECONDS))
+  if (stateMachine1.timeInCurrentState() < (initTimeout * MILLISECONDS_TO_SECONDS))
   {
     return;
   }
 
-  pumpStateMachine.transitionTo(offState);
+  stateMachine1.transitionTo(offState);
 }
 void initExitFunction()
 {
@@ -422,16 +543,16 @@ void offUpdateFunction()
 {
 
   // is minimum time up? no, then come back later
-  if (pumpStateMachine.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
+  if (stateMachine1.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
   {
     return;
   }
 
-  //if the temperature is lower than the threshold, transition to onState
+  //if the temperature is lower than the target, transition to onState
   // temperature is equal to INVALID at boot time until a valid temperature is measured
   if ((temperatureCurrent != INVALID) && (temperatureCurrent <= temperatureTarget - temperatureMargin))
   {
-    pumpStateMachine.transitionTo(onState);
+    stateMachine1.transitionTo(onState);
   }
 }
 void offExitFunction()
@@ -449,27 +570,117 @@ void onEnterFunction()
   // set the new state and publish the change
   setState(STATE_ON);
 
-  turnOnPump();
+  turnOnRelay(1);
 }
 void onUpdateFunction()
 {
 
   // is minimum time up? no, then come back later
-  if (pumpStateMachine.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
+  if (stateMachine1.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
   {
     return;
   }
 
-  //if the temperature is higher than the threshold, transition to offState
+  //if the temperature is higher than the target, transition to offState
   // temperature is equal to INVALID at boot time until a valid temperature is measured
   if ((temperatureCurrent != INVALID) && (temperatureCurrent >= temperatureTarget + temperatureMargin))
   {
-    pumpStateMachine.transitionTo(offState);
+    stateMachine1.transitionTo(offState);
   }
 }
 void onExitFunction()
 {
-  turnOffPump();
+  turnOffRelay(1);
+}
+
+/*******************************************************************************
+ * FSM state Name        : init2 
+ * Description           : when the system boots starts in this state to stabilize sensors.
+                            After some time (ten seconds), the system transitions to the idle state.
+* Status of the pump     : off
+*******************************************************************************/
+void initEnterFunction2()
+{
+  // set the new state and publish the change
+  setState2(STATE_INIT);
+}
+void initUpdateFunction2()
+{
+  // is time up? no, then come back later
+  if (stateMachine2.timeInCurrentState() < (initTimeout * MILLISECONDS_TO_SECONDS))
+  {
+    return;
+  }
+
+  stateMachine2.transitionTo(offState2);
+}
+void initExitFunction2()
+{
+}
+
+/*******************************************************************************
+ * FSM state Name        : off2 
+ * Description           : the system is checking the temperature. When it goes under a threshold, then
+                            the On state is triggered.
+ * Status of the pump    : off
+*******************************************************************************/
+void offEnterFunction2()
+{
+  // set the new state and publish the change
+  setState2(STATE_OFF);
+}
+void offUpdateFunction2()
+{
+
+  // is minimum time up? no, then come back later
+  if (stateMachine2.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
+  {
+    return;
+  }
+
+  //if the temperature2 is lower than the target2, transition to onState2
+  // temperature is equal to INVALID at boot time until a valid temperature is measured
+  if ((temperatureCurrent2 != INVALID) && (temperatureCurrent2 <= temperatureTarget2 - temperatureMargin))
+  {
+    stateMachine2.transitionTo(onState2);
+  }
+}
+void offExitFunction2()
+{
+}
+
+/*******************************************************************************
+ * FSM state Name        : On2 
+ * Description           : the pump will be turned on until the temperature reaches the target.
+                            It then transitions to the off state.
+ * Status of the pump    : on
+*******************************************************************************/
+void onEnterFunction2()
+{
+  // set the new state and publish the change
+  setState2(STATE_ON);
+
+  turnOnRelay(2);
+}
+void onUpdateFunction2()
+{
+
+  // is minimum time up? no, then come back later
+  if (stateMachine2.timeInCurrentState() < (minimumTimeInState * MILLISECONDS_TO_MINUTES))
+  {
+    return;
+  }
+
+  //if the temperature2 is higher than the target2, transition to offState2
+  // temperature is equal to INVALID at boot time until a valid temperature is measured
+  if ((temperatureCurrent2 != INVALID) && (temperatureCurrent2 >= temperatureTarget2 + temperatureMargin))
+  {
+    stateMachine2.transitionTo(offState2);
+  }
+}
+void onExitFunction2()
+{
+  turnOffRelay(2);
 }
 
 /*******************************************************************************
@@ -481,6 +692,23 @@ void onExitFunction()
 *******************************************************************************/
 
 /*******************************************************************************
+ * Function Name  : double2string
+ * Description    : return the string representation of the double number
+                     passed as parameter with 2 decimals
+ * Return         : the string
+ *******************************************************************************/
+String double2string(double doubleNumber)
+{
+  String stringNumber = String(doubleNumber);
+
+  //return only 2 decimals
+  // Example: show 19.00 instead of 19.000000
+  stringNumber = stringNumber.substring(0, stringNumber.length() - 4);
+
+  return stringNumber;
+}
+
+/*******************************************************************************
  * Function Name  : setState
  * Description    : sets the state of the system and publishes the change
  * Return         : none
@@ -488,29 +716,40 @@ void onExitFunction()
 void setState(String newState)
 {
   state = newState;
-  Particle.publish(APP_NAME, "Entering " + newState + " state", PRIVATE);
+  Particle.publish(APP_NAME, "FSM1 entering " + newState + " state", PRIVATE);
 }
 
 /*******************************************************************************
- * Function Name  : turnOnPump
- * Description    : turns on the pump, closing the relay
+ * Function Name  : setState2
+ * Description    : sets the state of the second FSM and publishes the change
  * Return         : none
  *******************************************************************************/
-void turnOnPump()
+void setState2(String newState)
 {
-  relayController.turnOnRelay(1);
-  Particle.publish(APP_NAME, "Turning on pump", PRIVATE);
+  state2 = newState;
+  Particle.publish(APP_NAME, "FSM2 entering " + newState + " state", PRIVATE);
 }
 
 /*******************************************************************************
- * Function Name  : turnOffPump
- * Description    : turns off the pump, by opening the relay
+ * Function Name  : turnOnRelay
+ * Description    : turns on the specified relay
  * Return         : none
  *******************************************************************************/
-void turnOffPump()
+void turnOnRelay(int relay)
 {
-  relayController.turnOffRelay(1);
-  Particle.publish(APP_NAME, "Turning off pump", PRIVATE);
+  relayController.turnOnRelay(relay);
+  Particle.publish(APP_NAME, "Turning on relay " + String(relay), PRIVATE);
+}
+
+/*******************************************************************************
+ * Function Name  : turnOffRelay
+ * Description    : turns off the specified relay
+ * Return         : none
+ *******************************************************************************/
+void turnOffRelay(int relay)
+{
+  relayController.turnOffRelay(relay);
+  Particle.publish(APP_NAME, "Turning off relay " + String(relay), PRIVATE);
 }
 
 /*******************************************************************************/
@@ -542,6 +781,8 @@ void readFromEeprom()
 
     temperatureTarget = myObj.temperatureTarget;
     temperatureCalibration = myObj.temperatureCalibration;
+    temperatureTarget2 = myObj.temperatureTarget2;
+    temperatureCalibration2 = myObj.temperatureCalibration2;
 
     Particle.publish(APP_NAME, "Read settings from EEPROM");
   }
@@ -559,6 +800,8 @@ void saveSettingsInEeprom()
   eepromMemory.version = EEPROM_VERSION;
   eepromMemory.temperatureTarget = temperatureTarget;
   eepromMemory.temperatureCalibration = temperatureCalibration;
+  eepromMemory.temperatureTarget2 = temperatureTarget2;
+  eepromMemory.temperatureCalibration2 = temperatureCalibration2;
 
   //then save
   EEPROM.put(EEPROM_ADDRESS, eepromMemory);
